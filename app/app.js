dec354932e5a5cf15a0a2027cf1c54e9271e6223
Konstantin Pankov <kspzel@gmail.com>
2019-05-21 12:24:44 +0300

    Copy from side project

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var fs = require('fs');
+var path = require('path');
+var convert = require('xml-js');
 
+var filePath;
+var currentData;
